228eaaaf98168a07e3f550fac31fcb2b9b2b972e
Fernando Mellone <fernandomellone2@gmail.com>
2022-07-12 15:55:22 -0400

    integrates firebase

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,15 @@
 <script>
-  import Router from "svelte-spa-router";
-  import routes from "./routes";
-  import Navbar from "./components/Navbar.svelte";
+  import Router from 'svelte-spa-router';
+  import routes from './routes';
+  import Navbar from './components/Navbar.svelte';
 </script>
 
 <Navbar />
 <main class="m-2">
-  <Router {routes} />
+  <Router
+    {routes}
+    on:routeLoaded={() => {
+      console.log('route loaded');
+    }}
+  />
 </main>
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
-import Home from "./pages/Home.svelte";
+import Home from './pages/Home.svelte';
 
 const routes = {
-  "/": Home
+  '/': Home,
 };
 
 export default routes;
